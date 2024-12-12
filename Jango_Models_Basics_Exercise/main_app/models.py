43bbf9982ed927a68ed59db984eca6189574c951
svetl <svetlin.sii@gmail.com>
2024-12-12 22:07:54 +0200

    added solutions

--- Jango_Models_Basics_Exercise/main_app/models.py
+++ Jango_Models_Basics_Exercise/main_app/models.py
@@ -5,3 +5,23 @@ class Person(models.Model):
 
     name = models.CharField(max_length=30)
     age = models.PositiveIntegerField()
+
+class Blog(models.Model):
+
+    post = models.TextField()
+    author = models.CharField(max_length=35)
+
+class WeatherForecast(models.Model):
+
+    date = models.DateField()
+    temperature = models.FloatField()
+    humidity = models.FloatField()
+    precipitation = models.FloatField()
+
+class Recipe(models.Model):
+
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField()
+    ingredients = models.TextField()
+    cook_time = models.PositiveIntegerField()
+    created_at = models.DateTimeField(auto_now=True)
