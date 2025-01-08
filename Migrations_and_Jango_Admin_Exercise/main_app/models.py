ab68ae89a994febfdeaae84f8637a472e07ab6ac
svetl <svetlin.sii@gmail.com>
2025-01-08 15:57:44 +0200

    initial commit

---
+++ Migrations_and_Jango_Admin_Exercise/main_app/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class Shoe(models.Model):
+
+    brand = models.CharField(max_length=25)
+    size = models.PositiveIntegerField()
+
+class UniqueBrands(models.Model):
+    brand = models.CharField(
+        max_length=25,
+        unique=True
+    )
