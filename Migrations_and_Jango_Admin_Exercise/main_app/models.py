ebb63128845a28af0fca08bfcb92b109371d09da
svetl <svetlin.sii@gmail.com>
2025-01-09 10:31:16 +0200

    added solution

--- Migrations_and_Jango_Admin_Exercise/main_app/models.py
+++ Migrations_and_Jango_Admin_Exercise/main_app/models.py
@@ -7,7 +7,16 @@ class Shoe(models.Model):
     size = models.PositiveIntegerField()
 
 class UniqueBrands(models.Model):
-    brand = models.CharField(
+    brand_name = models.CharField(
         max_length=25,
         unique=True
     )
+
+class EventRegistration(models.Model):
+
+    event_name = models.CharField(max_length=60)
+    participant_name = models.CharField(max_length=50)
+    registration_date = models.DateField()
+
+    def __str__(self):
+        return f"{self.participant_name} - {self.event_name}"
