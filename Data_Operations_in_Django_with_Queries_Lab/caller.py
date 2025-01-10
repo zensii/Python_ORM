7c1c6e3fb8368c4c49006512c416c62ee5995b3f
svetl <svetlin.sii@gmail.com>
2025-01-10 13:05:54 +0200

    initial commit

---
+++ Data_Operations_in_Django_with_Queries_Lab/caller.py
@@ -0,0 +1,47 @@
+import os
+import django
+from datetime import date
+
+# Set up Django
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
+django.setup()
+
+# Import your models here
+
+from main_app.models import Student
+
+def add_students():
+    new_employee = Student(
+        student_id='FC5204',
+        first_name='John',
+        last_name='Doe',
+        birth_date='1995-05-15',
+        email='john.doe@university.com'
+    )
+    new_employee.save()
+
+    Student.objects.create(
+        student_id='FE0054',
+        first_name='Jane',
+        last_name='Smith',
+        birth_date=None,
+        email='jane.smith@university.com'
+    )
+
+    student = Student()
+    student.student_id = 'FH2014'
+    student.first_name = 'Alice'
+    student.last_name = 'Johnson'
+    student.birth_date = '1998-02-10'
+    student.email = 'alice.johnson@university.com'
+    student.save()
+
+    Student.objects.create(
+        student_id='FH2015',
+        first_name='Bob',
+        last_name='Wilson',
+        birth_date='1996-11-25',
+        email='bob.wilson@university.com'
+    )
+
+# Run and print your queries
