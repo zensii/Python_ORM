79f892a764777b34fe9092577f3278b3b3622327
svetl <svetlin.sii@gmail.com>
2025-01-11 12:58:51 +0200

    added solution

--- Data_Operations_in_Django_with_Queries_Exercise/caller.py
+++ Data_Operations_in_Django_with_Queries_Exercise/caller.py
@@ -8,7 +8,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
 django.setup()
 
 # Import your models here
-from main_app.models import Pet
+from main_app.models import Pet,Artifact
 
 
 def create_pet(name: str, species: str):
@@ -16,3 +16,24 @@ def create_pet(name: str, species: str):
 
     return f"{name} is a very cute {species}!"
 # Create queries within functions
+
+def create_artifact(name: str, origin: str, age: int, description: str, is_magical: bool):
+
+    new_artifact = Artifact(
+        name=name,
+        origin=origin,
+        age=age,
+        description=description,
+        is_magical=is_magical
+    )
+    new_artifact.save()
+
+    return f"The artifact {name} is {age} years old!"
+
+def rename_artifact(artifact: Artifact, new_name: str):
+    if artifact.is_magical and artifact.age > 250:
+        artifact.name = new_name
+        artifact.save()
+
+def delete_all_artifacts():
+    Artifact.objects.all().delete()
--- Data_Operations_in_Django_with_Queries_Exercise/main_app/models.py
+++ Data_Operations_in_Django_with_Queries_Exercise/main_app/models.py
@@ -4,3 +4,12 @@ class Pet(models.Model):
     name = models.CharField(max_length=40)
     species = models.CharField(max_length=40)
 # Create your models here.
+
+
+class Artifact(models.Model):
+    name = models.CharField(max_length=70)
+    origin = models.CharField(max_length=70)
+    age = models.PositiveIntegerField()
+    description = models.TextField()
+    is_magical = models.BooleanField(default=False)
+
