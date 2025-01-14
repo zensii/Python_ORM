5a83b95070f8514af54dc09624d6efb0d70e99c6
svetl <svetlin.sii@gmail.com>
2025-01-14 15:58:39 +0200

    added solution

--- Working with Queries in Django - Exercise/caller.py
+++ Working with Queries in Django - Exercise/caller.py
@@ -11,7 +11,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
 django.setup()
 
 # Import your models
-from main_app.models import ArtworkGallery, Laptop
+from main_app.models import ArtworkGallery, Laptop, ChessPlayer
 
 
 # Create and check models
@@ -80,3 +80,32 @@ def update_operation_systems() -> None:
 
 def delete_inexpensive_laptops() -> None:
     Laptop.objects.filter(price__lt=1200).delete()
+
+
+def bulk_create_chess_players(args: List[ChessPlayer]):
+    ChessPlayer.objects.bulk_create(args)
+
+def delete_chess_players():
+    ChessPlayer.objects.filter(title='no title').delete()
+
+def change_chess_games_won():
+    ChessPlayer.objects.filter(title='GM').update(games_won=30)
+
+def change_chess_games_lost():
+    ChessPlayer.objects.filter(title='no title').update(games_lost=25)
+
+def change_chess_games_drawn():
+    ChessPlayer.objects.all().update(games_drawn=10)
+
+def grand_chess_title_GM():
+    ChessPlayer.objects.filter(rating__gte=2400).update(title='GM')
+
+def grand_chess_title_IM():
+    ChessPlayer.objects.filter(rating__range=(2300, 2399)).update(title='IM')
+
+def grand_chess_title_FM():
+    ChessPlayer.objects.filter(rating__range=(2200, 2299)).update(title='FM')
+
+def grand_chess_title_regular_player():
+    ChessPlayer.objects.filter(rating__range=(0, 2199)).update(title='regular player')
+
