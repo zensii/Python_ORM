42421b2fb1f1044175c4e794d74e9370c1357cbf
svetl <svetlin.sii@gmail.com>
2025-01-14 14:33:01 +0200

    added solutions

--- Working with Queries in Django - Exercise/caller.py
+++ Working with Queries in Django - Exercise/caller.py
@@ -1,33 +1,82 @@
 import os
+from typing import List
+
 import django
+from django.db.models import Case, When, Value
 
-
+from main_app.choices import OperationSystemChoice
 
 # Set up Django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
 django.setup()
 
 # Import your models
-from main_app.models import ArtworkGallery
+from main_app.models import ArtworkGallery, Laptop
+
+
 # Create and check models
 # Run and print your queries
 
+
 def show_highest_rated_art() -> str:
-    highest_rated = ArtworkGallery.objects.order_by('-rating', 'id').first()
-    return f"{highest_rated.art_name} is the highest-rated art with a {highest_rated.rating} rating!"
+    best_artwork = ArtworkGallery.objects.order_by('-rating', 'id').first()
+
+    return f"{best_artwork.art_name} is the highest-rated art with a {best_artwork.rating} rating!"
+
 
 def bulk_create_arts(first_art: ArtworkGallery, second_art: ArtworkGallery) -> None:
-    to_create = [first_art, second_art]
-    ArtworkGallery.objects.bulk_create(to_create)
+    ArtworkGallery.objects.bulk_create([
+        first_art,
+        second_art,
+    ])
+
 
 def delete_negative_rated_arts() -> None:
     ArtworkGallery.objects.filter(rating__lt=0).delete()
+    # DELETE FROM artwork WHERE rating < 0
 
 
-# artwork1 = ArtworkGallery(artist_name='Vincent van Gogh', art_name='Starry Night', rating=4, price=1200000.0)
-# artwork2 = ArtworkGallery(artist_name='Leonardo da Vinci', art_name='Mona Lisa', rating=5, price=1500000.0)
-#
-# # Bulk saves the instances
-# bulk_create_arts(artwork1, artwork2)
-# print(show_highest_rated_art())
-# print(ArtworkGallery.objects.all())
+def show_the_most_expensive_laptop() -> str:
+    most_expensive_laptop = Laptop.objects.order_by('-price', '-id').first()
+
+    return f"{most_expensive_laptop.brand} is the most expensive laptop available for {most_expensive_laptop.price}$!"
+
+
+def bulk_create_laptops(args: List[Laptop]) -> None:
+    Laptop.objects.bulk_create(args)
+
+
+def update_to_512_GB_storage() -> None:
+    """
+    UPDATE laptop
+    SET storage = 512
+    WHERE brand in (Asus, Lenovo)
+    """
+
+    Laptop.objects.filter(brand__in=("Asus", "Lenovo")).update(storage=512)
+
+
+def update_to_16_GB_memory() -> None:
+    Laptop.objects.filter(brand__in=("Apple", "Dell", "Acer")).update(memory=16)
+
+
+def update_operation_systems() -> None:
+    # Solution 1
+    Laptop.objects.update(
+        operation_system=Case(
+            When(brand="Asus", then=Value(OperationSystemChoice.WINDOWS)),
+            When(brand="Apple", then=Value(OperationSystemChoice.MACOS)),
+            When(brand__in=("Dell", "Acer"), then=Value(OperationSystemChoice.LINUX)),
+            When(brand="Lenovo", then=Value(OperationSystemChoice.CHROME_OS))
+        )
+    )
+
+    # Solution 2
+    # Laptop.objects.filter(brand="Asus").update(operation_system=OperationSystemChoice.WINDOWS)
+    # Laptop.objects.filter(brand="Apple").update(operation_system=OperationSystemChoice.MACOS)
+    # Laptop.objects.filter(brand__in=("Dell", "Acer")).update(operation_system=OperationSystemChoice.LINUX)
+    # Laptop.objects.filter(brand="Lenovo").update(operation_system=OperationSystemChoice.CHROME_OS)
+
+
+def delete_inexpensive_laptops() -> None:
+    Laptop.objects.filter(price__lt=1200).delete()
--- Working with Queries in Django - Exercise/main_app/models.py
+++ Working with Queries in Django - Exercise/main_app/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from main_app.choices import OperationSystemChoice, LaptopBrandChoice
+
 
 # Create your models here.
 
@@ -63,7 +65,46 @@ class Workout(models.Model):
 
 
 class ArtworkGallery(models.Model):
-    artist_name = models.CharField(max_length=100)
-    art_name = models.CharField(max_length=100)
+    artist_name = models.CharField(
+        max_length=100,
+    )
+
+    art_name = models.CharField(
+        max_length=100,
+    )
+
     rating = models.IntegerField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+    )
+
+
+class Laptop(models.Model):
+    brand = models.CharField(
+        max_length=20,
+        choices=LaptopBrandChoice.choices
+    )
+
+    processor = models.CharField(
+        max_length=100,
+    )
+
+    memory = models.PositiveIntegerField(
+        help_text="Memory in GB",
+    )
+
+    storage = models.PositiveIntegerField(
+        help_text="Storage in GB",
+    )
+
+    operation_system = models.CharField(
+        max_length=20,
+        choices=OperationSystemChoice.choices,
+    )
+
+    price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+    )
