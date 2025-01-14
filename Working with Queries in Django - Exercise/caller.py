f49713cb7705b71b14dd4dd48ff4cc4ff8a8b1b7
svetl <svetlin.sii@gmail.com>
2025-01-14 16:33:12 +0200

    added solution

--- Working with Queries in Django - Exercise/caller.py
+++ Working with Queries in Django - Exercise/caller.py
@@ -11,7 +11,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
 django.setup()
 
 # Import your models
-from main_app.models import ArtworkGallery, Laptop, ChessPlayer
+from main_app.models import ArtworkGallery, Laptop, ChessPlayer, Meal
 
 
 # Create and check models
@@ -108,4 +108,35 @@ def grand_chess_title_FM():
 
 def grand_chess_title_regular_player():
     ChessPlayer.objects.filter(rating__range=(0, 2199)).update(title='regular player')
+
+def set_new_chefs():
+    Meal.objects.update(
+        chef=Case(
+            When(meal_type='Breakfast', then=Value('Gordon Ramsay')),
+            When(meal_type='Lunch', then=Value('Julia Child')),
+            When(meal_type='Dinner', then=Value('Jamie Oliver')),
+            When(meal_type='Snack', then=Value('Thomas Keller')),
+        )
+    )
+
+def set_new_preparation_times():
+    Meal.objects.update(
+        preparation_time=Case(
+            When(meal_type='Breakfast', then=Value('10 minutes')),
+            When(meal_type='Lunch', then=Value('12 minutes')),
+            When(meal_type='Dinner', then=Value('15 minutes')),
+            When(meal_type='Snack', then=Value('5 minutes')),
+        )
+    )
+
+def update_low_calorie_meals():
+    Meal.objects.filter(meal_type__in=['Breakfast', 'Dinner']).update(calories=400)
+
+
+def update_high_calorie_meals():
+    Meal.objects.filter(meal_type__in=['Lunch', 'Snack']).update(calories=700)
+
+
+def delete_lunch_and_snack_meals():
+    Meal.objects.filter(meal_type__in=['Lunch', 'Snack']).delete()
 
