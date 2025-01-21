0675c7e41b8d7b5fc276b0986da50f390dd956b8
svetl <svetlin.sii@gmail.com>
2025-01-21 13:17:40 +0200

    added solution

--- Model_Inheritance_and_Customization_Exercise/main_app/models.py
+++ Model_Inheritance_and_Customization_Exercise/main_app/models.py
@@ -42,3 +42,48 @@ class VengeanceDemonHunter(DemonHunter):
 
 class FelbladeDemonHunter(DemonHunter):
     felblade_ability = models.CharField(max_length=100)
+
+class UserProfile(models.Model):
+    username = models.CharField(max_length=70)
+    email = models.EmailField()
+    bio = models.TextField(blank=True, null=True)
+
+class Message(models.Model):
+    sender = models.ForeignKey(to=UserProfile, related_name='sent_messages', on_delete=models.CASCADE)
+    receiver = models.ForeignKey(to=UserProfile, related_name='received_messages', on_delete=models.CASCADE)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now=True)
+    is_read = models.BooleanField(default=False)
+
+    def mark_as_read(self):
+        self.is_read = True
+
+    def reply_to_message(self, reply_content: str):
+        new_message = Message(
+            sender=self.receiver,
+            receiver=self.sender,
+            content=reply_content
+        )
+        new_message.save()
+        return new_message
+
+    def forward_message(self, receiver: UserProfile):
+        new_message = Message(
+            sender=self.receiver,
+            receiver=receiver,
+            content=self.content
+        )
+        new_message.save()
+        return new_message
+
+
+
+
+
+
+
+
+
+
+
+
