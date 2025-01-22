e6fe14ef8e47be741c8851c007ad397b444066a0
svetl <svetlin.sii@gmail.com>
2025-01-22 12:58:29 +0200

    added solution

--- Model_Inheritance_and_Customization_Exercise/caller.py
+++ Model_Inheritance_and_Customization_Exercise/caller.py
@@ -1,4 +1,6 @@
 import os
+from datetime import date
+
 import django
 from django.core.exceptions import ValidationError
 
@@ -7,7 +9,8 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
 django.setup()
 
 # Import your models here
-from main_app.models import Mage, Necromancer, UserProfile, Message, Student, CreditCard
+from main_app.models import Mage, Necromancer, UserProfile, Message, Student, CreditCard, SpecialReservation, Hotel, \
+    Room
 
 # # Create queries within functions
 # # Create instances
@@ -93,3 +96,46 @@ from main_app.models import Mage, Necromancer, UserProfile, Message, Student, Cr
 # for credit_card in credit_cards:
 #     print(f"Card Owner: {credit_card.card_owner}")
 #     print(f"Card Number: {credit_card.card_number}")
+# Create a Hotel instance
+hotel = Hotel.objects.create(name="Hotel ABC", address="123 Main St")
+
+# Create Room instances associated with the hotel
+room1 = Room.objects.create(
+    hotel=hotel,
+    number="102",
+    capacity=2,
+    total_guests=1,
+    price_per_night=100.00
+)
+
+# Create SpecialReservation instance
+special_reservation1 = SpecialReservation(
+    room=room1,
+    start_date=date(2023, 1, 1),
+    end_date=date(2023, 1, 5)
+)
+
+# Create a special reservation1
+print(special_reservation1.save())
+
+# Create SpecialReservation instance
+special_reservation2 = SpecialReservation(
+    room=room1,
+    start_date=date(2023, 1, 10),
+    end_date=date(2023, 1, 12)
+)
+
+# Create a special reservation2
+print(special_reservation2.save())
+
+# Calculate total cost for special reservation1
+print(special_reservation1.calculate_total_cost())
+
+# Calculate reservation period for special reservation1
+print(special_reservation1.reservation_period())
+
+# Example of extending a SpecialReservation
+try:
+    special_reservation1.extend_reservation(5)
+except ValidationError as e:
+    print(e)
--- Model_Inheritance_and_Customization_Exercise/main_app/models.py
+++ Model_Inheritance_and_Customization_Exercise/main_app/models.py
@@ -1,6 +1,10 @@
+from ctypes.wintypes import HPALETTE
+from datetime import datetime, timedelta
+
 from django.core.exceptions import ValidationError
 from django.db import models
 from django.db.models import TextField, IntegerField
+from django.templatetags.tz import datetimeobject
 
 
 # Create your models here.
@@ -128,8 +132,93 @@ class CreditCard(models.Model):
     card_number = MaskedCreditCardField(max_length=20)
 
 
+class Hotel(models.Model):
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=200)
+
+class Room(models.Model):
+
+    hotel = models.ForeignKey(to=Hotel, on_delete=models.CASCADE)
+    number = models.CharField(max_length=100, unique=True)
+    capacity = models.PositiveIntegerField()
+    total_guests = models.PositiveIntegerField()
+    price_per_night = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def clean(self):
+
+        if self.total_guests > self.capacity:
+            raise ValidationError("Total guests are more than the capacity of the room")
+
+    def save(self, *args, **kwargs):
+
+        self.clean()
+        super().save(*args, **kwargs)
+        return f"Room {self.number} created successfully"
 
 
+class BaseReservation(models.Model):
 
+    room = models.ForeignKey(to=Room, on_delete=models.CASCADE)
+    start_date = models.DateField()
+    end_date = models.DateField()
+
+
+    @property
+    def is_reserved(self):
+        room_reserved = self.__class__.objects.filter(
+            room= self.room,
+            start_date__range=[self.start_date, self.end_date],
+            end_date__range=[self.start_date, self.end_date]
+        )
+
+        return room_reserved.exists()
+
+    def clean(self):
+        if self.start_date >= self.end_date:
+            raise ValidationError("Start date cannot be after or in the same end date")
+
+        if self.is_reserved:
+            raise ValidationError(f"Room {self.room.number} cannot be reserved")
+
+    def reservation_period(self):
+        duration = self.end_date - self.start_date
+        return duration.days
+
+    def calculate_total_cost(self):
+        price = self.reservation_period() * self.room.price_per_night
+        return round(price, 2)
+
+    class Meta:
+        abstract=True
+
+class RegularReservation(BaseReservation):
+
+    def save(self, *args, **kwargs):
+        super().clean()
+        super().save(*args, **kwargs)
+
+        return f"Regular reservation for room {self.room.number}"
+
+class SpecialReservation(BaseReservation):
+
+    def save(self, *args, **kwargs):
+        super().clean()
+        super().save(*args, **kwargs)
+
+        return f"Special reservation for room {self.room.number}"
+
+    def extend_reservation(self, days: int):
+
+        new_end_date = self.end_date + timedelta(days=days)
+        room_reserved = self.__class__.objects.filter(
+            room=self.room,
+            start_date__gt=self.end_date,
+            start_date__lte=new_end_date
+        )
+
+        if room_reserved.exists():
+            raise ValidationError("Error during extending reservation")
+
+        return f"Extended reservation for room {self.room.number} with {days} days"
 
 
