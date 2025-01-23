a9be662d691c99fcfdc7102296ca8fd84eb95c97
svetl <svetlin.sii@gmail.com>
2025-01-23 13:32:34 +0200

    Added solutions

--- Advanced_Django_Model_Techniques_Lab/main_app/models.py
+++ Advanced_Django_Model_Techniques_Lab/main_app/models.py
@@ -48,7 +48,20 @@ class Menu(models.Model):
     )
     restaurant = models.ForeignKey(to=Restaurant, on_delete=models.CASCADE)
 
-class RestaurantReview(models.Model):
+class ReviewMixin(models.Model):
+
+    review_content = models.TextField()
+    rating = models.PositiveIntegerField(
+        validators=[
+            MaxValueValidator(5)
+        ]
+    )
+
+    class Meta:
+        abstract=True
+        ordering = ('-rating',)
+
+class RestaurantReview(ReviewMixin):
 
     reviewer_name = models.CharField(
         max_length=100,
@@ -56,12 +69,6 @@ class RestaurantReview(models.Model):
     restaurant = models.ForeignKey(
         to=Restaurant,
         on_delete=models.CASCADE,
-    )
-    review_content = models.TextField()
-    rating = models.PositiveIntegerField(
-        validators=[
-            MaxValueValidator(5)
-        ]
     )
 
     class Meta:
@@ -85,3 +92,22 @@ class FoodCriticRestaurantReview(RestaurantReview):
         verbose_name = "Food Critic Review"
         verbose_name_plural = "Food Critic Reviews"
         unique_together = ('reviewer_name', 'restaurant')
+
+class MenuReview(ReviewMixin):
+
+    reviewer_name = models.CharField(
+        max_length=100,
+    )
+    menu = models.ForeignKey(
+        to=Menu,
+        on_delete=models.CASCADE,
+    )
+
+    class Meta:
+        ordering = ('-rating',)
+        verbose_name = 'Menu Review'
+        verbose_name_plural = 'Menu Reviews'
+        unique_together = ('reviewer_name', 'menu')
+        indexes = [
+            models.Index(fields=['menu'], name='main_app_menu_review_menu_id')
+        ]
