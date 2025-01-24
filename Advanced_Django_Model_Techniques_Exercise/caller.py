ec723a65921da5c5368468192617d82515d61ebb
svetl <svetlin.sii@gmail.com>
2025-01-24 13:51:37 +0200

    added a solution

--- Advanced_Django_Model_Techniques_Exercise/caller.py
+++ Advanced_Django_Model_Techniques_Exercise/caller.py
@@ -8,7 +8,7 @@ from django.core.exceptions import ValidationError
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
 django.setup()
 
-from main_app.models import Customer
+from main_app.models import Customer, Book
 
 # customer = Customer(
 #     name="Svetlin Nakov1",
@@ -24,3 +24,20 @@ from main_app.models import Customer
 #
 # except ValidationError as e:
 #     print('\n'.join(e.messages))
+
+book = Book(
+    title="Short Title",
+    description="A book with a short title.",
+    genre="Fiction",
+    author="A",
+    isbn="1234"
+)
+
+try:
+    book.full_clean()
+    book.save()
+
+except ValidationError as e:
+    print("Validation Error for Book:")
+    for field, errors in e.message_dict.items():
+        print(f"{field}: {', '.join(errors)}")
